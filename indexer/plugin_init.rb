63a868505fd1e39c7f354049e528d9cb4d4e8141
Mark Cooper <markchristophercooper@gmail.com>
2017-01-18 08:17:16 -0800

    Update example config pending lookup

--- indexer/plugin_init.rb
+++ indexer/plugin_init.rb
@@ -3,10 +3,10 @@ Dir.glob(File.join(File.dirname(__FILE__), "lib", "*.rb")).sort.each do |file|
   require File.absolute_path(file)
 end
 
-# aspace-islandora config
+# aspace-islandora config (example)
 AppConfig[:islandora_config] = {
-  base_url:  ENV.fetch("ISLANDORA_BASE_URL"),
-  rest_path: ENV.fetch("ISLANDORA_REST_PATH"),
-  username:  ENV.fetch("ISLANDORA_USERNAME"),
-  password:  ENV.fetch("ISLANDORA_PASSWORD"),
+  base_url:  ENV.fetch("ISLANDORA_BASE_URL", nil),
+  rest_path: ENV.fetch("ISLANDORA_REST_PATH", nil),
+  username:  ENV.fetch("ISLANDORA_USERNAME", nil),
+  password:  ENV.fetch("ISLANDORA_PASSWORD", nil),
 }
