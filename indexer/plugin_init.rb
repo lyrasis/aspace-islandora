a123bc48c1a462df31c082a81d3cab3e0fef6c45
Mark Cooper <markchristophercooper@gmail.com>
2017-01-25 10:41:04 -0800

    Check for islandora_config before defining default

--- indexer/plugin_init.rb
+++ indexer/plugin_init.rb
@@ -3,11 +3,13 @@ Dir.glob(File.join(File.dirname(__FILE__), "lib", "*.rb")).sort.each do |file|
   require File.absolute_path(file)
 end
 
-# aspace-islandora config (example)
-AppConfig[:islandora_config] = {
-  base_url:  ENV.fetch("ISLANDORA_BASE_URL", nil),
-  rest_path: ENV.fetch("ISLANDORA_REST_PATH", nil),
-  username:  ENV.fetch("ISLANDORA_USERNAME", nil),
-  password:  ENV.fetch("ISLANDORA_PASSWORD", nil),
-  api_key:   ENV.fetch("ISLANDORA_API_KEY", nil),
-}
+unless AppConfig.has_key?(:islandora_config)
+  # aspace-islandora config (example)
+  AppConfig[:islandora_config] = {
+    base_url:  ENV.fetch("ISLANDORA_BASE_URL", nil),
+    rest_path: ENV.fetch("ISLANDORA_REST_PATH", nil),
+    username:  ENV.fetch("ISLANDORA_USERNAME", nil),
+    password:  ENV.fetch("ISLANDORA_PASSWORD", nil),
+    api_key:   ENV.fetch("ISLANDORA_API_KEY", nil),
+  }
+end
